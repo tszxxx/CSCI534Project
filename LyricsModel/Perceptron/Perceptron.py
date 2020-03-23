985d44ae9fea8fe3494b549002136be97ee0f8b9
Hangjie <hangjiez@usc.edu>
2020-03-23 11:39:47 -0700

    add perceptron

--- LyricsModel/Perceptron/Perceptron.py
+++ LyricsModel/Perceptron/Perceptron.py
@@ -1,190 +1,123 @@
-import sys
-import os
-import re
+import csv
+import chardet
 import string
-import time
+import json
 from random import shuffle
 
-vanilla_pn_dict, average_pn_dict = {}, {}
-vanilla_td_bias, average_td_bias = 0, 0
-vanilla_td_dict, average_td_dict = {}, {}
-vanilla_pn_bias, average_pn_bias = 0, 0
-all_train_data = {'positive':[], 'negative':[], 'truthful':[], 'deceptive':[]}
+table = str.maketrans('', '', string.punctuation)
 stop_words = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'} 
 
-def get_train_content(file_path, positive_negative, truthful_deceptive):
-    global all_train_data, stop_words
+def get_train_content(train_file_path, train_dir_path):
+    global stop_words, table
+    arousal_train_data = [[], []]
+    valence_train_data = [[], []]
+    train_dir_path += '/'
+    with open(train_file_path, 'r', encoding='utf-8') as train_file:
+        cnt = 0
+        spamreader = csv.reader(train_file, delimiter=',', quotechar='\"')
+        for records in spamreader:
+            if cnt > 0:
+                input_file_path = train_dir_path + records[0] + '.txt'
+                all_token = []
+                encoding = ''
+                with open(input_file_path, 'rb') as input_file:
+                    data = input_file.read()
+                    encoding = chardet.detect(data)
+                with open(input_file_path, 'r', encoding=encoding['encoding']) as input_file:
+                    for line in input_file:
+                        tokens = line.split()
+                        for token in tokens:
+                            token = token.translate(table).lower().strip()
+                            if len(token) > 0 and token not in stop_words and token not in all_token:
+                                all_token.append(token)
+                mood = records[3].lower()
+                if mood == 'happy':
+                    arousal_train_data[1].append(all_token)
+                    valence_train_data[1].append(all_token)
+                if mood == 'angry':
+                    arousal_train_data[1].append(all_token)
+                    valence_train_data[0].append(all_token)
+                if mood == 'sad':
+                    arousal_train_data[0].append(all_token)
+                    valence_train_data[0].append(all_token)
+                if mood == 'relaxed':
+                    arousal_train_data[0].append(all_token)
+                    valence_train_data[1].append(all_token)
+            cnt += 1
 
-    all_token = []
-    file = open(file_path, 'r')
-    line = file.readline()
-    table = str.maketrans('','',string.punctuation)
+    return arousal_train_data, valence_train_data
 
-    while line!='':
-        tokens = re.split(r'[\.|\,|\ ]', line)
-        for token in tokens:
-            lowercase_token = token.translate(table).strip('\n')
-            if lowercase_token not in all_token and lowercase_token != '' and lowercase_token not in stop_words and not re.search(r'\d', lowercase_token):
-                all_token.append(lowercase_token)
-        line = file.readline()
-    file.close()
-
-    all_train_data[positive_negative].append(all_token)
-    all_train_data[truthful_deceptive].append(all_token)
-
-def train_perceptron_perfile_truthful_deceptive(tokens, truthful_deceptive, iter_time):
-    global vanilla_td_dict, average_td_dict
-    global vanilla_td_bias, average_td_bias
-    res_td = vanilla_td_bias
+def train_perceptron_per_file(tokens, classifer, vanilla_dict, vanilla_bias, average_dict, average_bias, iter_time):
+    res = vanilla_bias
     for token in tokens:
-        res_td += vanilla_td_dict.setdefault(token, 0)
-    if truthful_deceptive == 'truthful':
-        if res_td <= 0:
-            average_td_bias += iter_time
-            vanilla_td_bias += 1
+        res += vanilla_dict.setdefault(token, 0)
+    if classifer == 1:
+        if res <= 0:
+            average_bias += iter_time
+            vanilla_bias += 1
             for token in tokens:
-                vanilla_td_dict[token] = vanilla_td_dict.setdefault(token, 0) + 1
-                average_td_dict[token] = average_td_dict.setdefault(token, 0) + iter_time
+                vanilla_dict[token] = vanilla_dict.get(token, 0) + 1
+                average_dict[token] = average_dict.get(token, 0) + iter_time
     else:
-        if res_td >= 0:
-            average_td_bias -= iter_time
-            vanilla_td_bias -= 1
+        if res >= 0:
+            average_bias -= iter_time
+            vanilla_bias -= 1
             for token in tokens:
-                vanilla_td_dict[token] = vanilla_td_dict.setdefault(token, 0) - 1
-                average_td_dict[token] = vanilla_td_dict.setdefault(token, 0) - iter_time
+                vanilla_dict[token] = vanilla_dict.get(token, 0) - 1
+                average_dict[token] = vanilla_dict.get(token, 0) - iter_time
+    return vanilla_bias, average_bias
 
-def train_perceptron_perfile_positive_negative(tokens, positive_negative, iter_time):
-    global vanilla_pn_dict, average_pn_dict
-    global vanilla_pn_bias, average_pn_bias
-    res_pn = vanilla_pn_bias
-    for token in tokens:
-        res_pn += vanilla_pn_dict.setdefault(token, 0)
-    if positive_negative == 'positive':
-        if res_pn <= 0:
-            average_pn_bias += iter_time
-            vanilla_pn_bias += 1
-            for token in tokens:
-                vanilla_pn_dict[token] = vanilla_pn_dict.setdefault(token, 0) + 1
-                average_pn_dict[token] = average_pn_dict.setdefault(token, 0) + iter_time
-    else:
-        if res_pn >= 0:
-            average_pn_bias -= iter_time
-            vanilla_pn_bias -= 1
-            for token in tokens:
-                vanilla_pn_dict[token] = vanilla_pn_dict.setdefault(token, 0) - 1
-                average_pn_dict[token] = vanilla_pn_dict.setdefault(token, 0) - iter_time
-
-def train_perceptron_once(iter_time):
+def train_perceptron_once(train_data, vanilla_dict, vanilla_bias, average_dict, average_bias, iter_time):
     global all_train_data
-    shuffle(all_train_data['positive'])
-    shuffle(all_train_data['negative'])
-    shuffle(all_train_data['truthful'])
-    shuffle(all_train_data['deceptive'])
-    size = len(all_train_data['positive'])
+    shuffle(train_data[0])
+    shuffle(train_data[1])
+    size = len(train_data[0])
     iter_time = iter_time * 2 * size
-    notChange = True
 
     for i in range(size):
-        res = train_perceptron_perfile_positive_negative(all_train_data['positive'][i], 'positive', iter_time + 2 * i)
-        notChange = notChange and res
+        vanilla_bias, average_bias = train_perceptron_per_file(train_data[0][i], 0, vanilla_dict, vanilla_bias, average_dict, average_bias, iter_time + 2 * i)
+        vanilla_bias, average_bias = train_perceptron_per_file(train_data[1][i], 1, vanilla_dict, vanilla_bias, average_dict, average_bias, iter_time + 2 * i + 1)
 
-        res =  train_perceptron_perfile_positive_negative(all_train_data['negative'][i], 'negative', iter_time + 2 * i + 1)
-        notChange = notChange and res
-
-        res =  train_perceptron_perfile_truthful_deceptive(all_train_data['truthful'][i], 'truthful', iter_time + 2 * i)
-        notChange = notChange and res
-
-        res =  train_perceptron_perfile_truthful_deceptive(all_train_data['deceptive'][i], 'deceptive', iter_time + 2 * i + 1)
-        notChange = notChange and res
-    
-    return notChange
-
-def train_perceptron(Maxiter):
-    global all_train_data
-    size = len(all_train_data['positive'])
+def train_perceptron(train_data, Maxiter):
+    vanilla_dict, average_dict = {}, {}
+    vanilla_bias, average_bias = 0, 0
     for i in range(Maxiter):
-        if train_perceptron_once(i):
-            break
-    return (i + 1) * 2 * size
+        train_perceptron_once(train_data, vanilla_dict, vanilla_bias, average_dict, average_bias, i)
+    return vanilla_dict, vanilla_bias, average_dict, average_bias
 
-def output_vanilla(file):
-    global vanilla_pn_dict, vanilla_pn_bias
-    file.write('for positive/negative\n')
-    file.write('bias:\t' + str(vanilla_pn_bias) + '\n')
-    file.write('token' + '\t' + 'coefficient' + '\n')
-    for token in vanilla_pn_dict:
-        coefficient = vanilla_pn_dict[token]
+def output_vanilla(output_file_path, vanilla_dict, vanilla_bias):
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        keys = list(vanilla_dict.keys())
+        for token in keys:
+            if vanilla_dict[token] == 0:
+                del vanilla_dict[token]
+        output_file.write(json.dumps(vanilla_dict))
+        output_file.write('\n')
+        output_file.write(str(vanilla_bias))
+
+def output_average(output_file_path, average_dict, average_bias, vanilla_dict, vanilla_bias, total_samples):
+    final_average_dict = {}
+    final_average_bias = vanilla_bias - average_bias / total_samples
+    for token in average_dict:
+        coefficient = vanilla_dict[token] - average_dict[token] / total_samples
         if coefficient != 0:
-            file.write(token + '\t' + str(coefficient) + '\n')
-            
-    global vanilla_td_dict, vanilla_td_bias
-    file.write('for truthful/deceptive\n')
-    file.write('bias:\t' + str(vanilla_td_bias) + '\n')
-    file.write('token'+'\t'+'coefficient'+'\n')
-    for token in vanilla_td_dict:
-        coefficient = vanilla_td_dict[token]
-        if coefficient != 0:
-            file.write(token + '\t' + str(coefficient) + '\n')
-            
-def output_average(file, total_samples):
-    global average_pn_dict, average_pn_bias
-    global vanilla_pn_dict, vanilla_pn_bias
-    file.write('for positive/negative\n')
-    file.write('bias:\t' + str(vanilla_pn_bias - average_pn_bias / total_samples) + '\n')
-    file.write('token' + '\t' +'coefficient\n')
-    for token in average_pn_dict:
-        coefficient = vanilla_pn_dict[token] - average_pn_dict[token] / total_samples
-        if coefficient != 0:
-            file.write(token + '\t' + str(coefficient) + '\n')
-            
-    global average_td_dict, average_td_bias
-    global vanilla_td_dict, vanilla_td_bias
-    file.write('for truthful/deceptive\n')
-    file.write('bias:\t' + str(vanilla_td_bias - average_td_bias / total_samples) + '\n')
-    file.write('token' + '\t' +'coefficient\n')
-    for token in average_td_dict:
-        coefficient = vanilla_td_dict[token] - average_td_dict[token] / total_samples
-        if coefficient != 0:
-            file.write(token + '\t' + str(coefficient) + '\n')
+            final_average_dict[token] = coefficient
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(json.dumps(final_average_dict))
+        output_file.write('\n')
+        output_file.write(str(final_average_bias))
 
 if __name__ == '__main__':
-    a = time.time()
-    input_path = sys.argv[1]
-    test_path_list = []
-    for root,dirs,files in os.walk(input_path):
-        for file in files:
-            file_path = os.path.join(root,file)
-            if file_path.find('.txt') != -1:
-                if file_path.find('negative') != -1:
-                    if file_path.find('deceptive') != -1:
-                        if file_path.find('fold1') == -1:
-                            get_train_content(file_path, 'negative', 'deceptive')
-                        else:
-                            test_path_list.append([file_path, 'negative', 'deceptive'])
-                    elif file_path.find('truthful') != -1:
-                        if file_path.find('fold1') == -1:
-                            get_train_content(file_path, 'negative', 'truthful')
-                        else:
-                            test_path_list.append([file_path, 'negative', 'truthful'])
-                elif file_path.find('positive') != -1:
-                    if file_path.find('deceptive') != -1:
-                        if file_path.find('fold1') == -1:
-                            get_train_content(file_path, 'positive', 'deceptive')
-                        else:
-                            test_path_list.append([file_path, 'positive', 'deceptive'])
-                    elif file_path.find('truthful') != -1:
-                        if file_path.find('fold1') == -1:
-                            get_train_content(file_path, 'positive', 'truthful')
-                        else:
-                            test_path_list.append([file_path, 'positive', 'truthful'])
+    train_balanced_file_path = '../train/MoodyLyrics/ml_balanced.csv'
+    train_dir_path = '../train/words'
+    arousal_train_data, valence_train_data = get_train_content(train_balanced_file_path, train_dir_path)
 
-    total_samples = train_perceptron(100)
-    b = time.time()
+    arousal_vanilla_dict, arousal_vanilla_bias, arousal_average_dict, arousal_average_bias \
+        = train_perceptron(arousal_train_data, 100)
+    valence_vanilla_dict, valence_vanilla_bias, valence_average_dict, valence_average_bias \
+        = train_perceptron(valence_train_data, 100)
 
-    output_file = open("vanillamodel.txt","w")    
-    output_vanilla(output_file)
-    output_file.close()
-
-    output_file = open("averagedmodel.txt","w")    
-    output_average(output_file, total_samples)
-    output_file.close()
+    output_vanilla('arousal_vanillamodel.txt', arousal_vanilla_dict, arousal_vanilla_bias)
+    output_average('arousal_averagedmodel.txt', arousal_vanilla_dict, arousal_average_bias, arousal_vanilla_dict, arousal_vanilla_bias, 100 * 2 * len(arousal_train_data[0]))
+    output_vanilla('valence_vanillamodel.txt', valence_vanilla_dict, valence_vanilla_bias)
+    output_average('valence_averagedmodel.txt', valence_vanilla_dict, valence_average_bias, valence_vanilla_dict, valence_vanilla_bias, 100 * 2 * len(valence_train_data[0]))
